2275851978c30d05cb9769be9f2a9b9bdf6c7de4
James Sherman <katharinesherman@Jamess-MacBook-Air-2.local>
2020-05-07 15:27:23 -0400

    commit

--- graph.js
+++ graph.js
@@ -43,7 +43,7 @@ var graph =
     createLabels(screen,margins,graph,target);
     createAxes(screen,margins,graph,target,xScale,yScaleL,yScaleR)
     drawLines(countrys,graph,target,xScale,yScaleL,yScaleR,yearscale)
-    createLegend(screen,margins,graph,target,yearscale)
+   
     
 }
 
@@ -122,6 +122,9 @@ var drawLines = function(countrys,graph,target,xScale,yScaleL,yScaleR, yearScale
         .x(function(box){return xScale(box.year)})
         .y(function(box){return yScaleR(box.pvaccinated)})
         .curve(d3.curveCardinal)
+     var mycolor = d3.scaleOrdinal()
+     .domain(["Senegal", "Bolivia", "United States", "Mexico", "Cambodia"])
+     .range(d3.schemeSet1)
      
      var lines = 
         target.select(".graph")
@@ -133,101 +136,21 @@ var drawLines = function(countrys,graph,target,xScale,yScaleL,yScaleR, yearScale
         .attr("fill","none")
         .attr("stroke",function(country)
                 {
-           
-            return subject.country;
+            return "black"
             
         })
-        .classed("line",true)
-        .attr("fill","none")
-        .attr("stroke",function(countrys) 
-        { 
-            return bloodScale(subject.country);
-        })
-        .attr("stroke-width",3)
-        .on("mouseover",function(countrys)
-        {   
-            if(! d3.select(this).classed("off"))
-            {
-            d3.selectAll(".line")
-            .classed("fade",true);
-            
-            d3.select(this)
-                .classed("fade",false)
-                .raise(); //move to top
-            }
-        })
-        .on("mouseout",function(countrys)
-           {
-            if(! d3.select(this).classed("off"))
-            {
-            
-            d3.selectAll(".line")
-                .classed("fade",false);
-            }
    
         
         lines.append("path")
         .datum(function(country)
         {return country.yeardata})
         .attr("d",lineGenerator1)
+        .style ("stroke", function (d){return mycolor(d.name)}) 
      
      lines.append("path")
         .datum(function(country)
         {return country.yeardata})
         .attr("d",lineGenerator2)
          .style("stroke-dasharray", ("3, 3"))
+         .style ("stroke", function (d){return mycolor(d.name)}) 
         }
-
-var createLegend = function(screen,margins,graph,
-                             target,yearscale)
-{
-        var legend = d3.select(target)
-        .append("g")
-        .classed("legend",true)
-        .attr("transform","translate("+
-              (margins.left+ 10) +","+
-             (margins.top+10)+")");
-    
-    var entries = legend.selectAll("g")
-        .data(["Senegal","Bolivia","Cambodia","Mexico", "United States"])
-        .enter()
-        .append("g")
-        .classed("legendEntry",true)
-        .attr("fill",function(country)
-              
-              {
-                return yearScale(country);
-             })
-            .attr("transform",function(country,index)
-                  {
-                return "translate(0,"+index*20+")";
-              })
-        .on("click",function(country)
-            
-            var on = ! d3.select(this).classed("off");
-            if(on) //turn off
-                {
-                    d3.select(this)
-                        .classed("off",true);
-                    d3.selectAll("."+country)
-                        .classed("off",true);
-                }
-            else
-                {
-                    d3.select(this)
-                        .classed("off",false);        
-                    d3.selectAll("."+country)
-                        .classed("off",false);
-
-                }
-        })
-            
-        entries.append("rect")
-                .attr("width",10)
-                .attr("height",10)
-    
-        entries.append("text")
-                .text(function(country){return country;})
-                .attr("x",15)
-                .attr("y",10)  
-}
